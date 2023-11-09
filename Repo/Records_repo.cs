f6893006af70866373e6e51efbf1271ee1f6686b
philipv99 <philipv9913@gmail.com>
2023-11-09 13:24:11 +0100

    try agina

--- Repo/Records_repo.cs
+++ Repo/Records_repo.cs
@@ -12,5 +12,10 @@ namespace UserStorieProject_School_prog.Repo
             Records = new List<Record_class>() 
             { new Record_class() { Title = "man", Artist = "him", Duration = 200, Id = 1, PublicationYear = 2002 } };
         }
+
+        private List<Record_class> Get()
+        {
+            return Records;
+        }
      }
 }
